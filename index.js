ed9b7c87dc215bbb91e669400435d1d3eb1d7803
adriiglezs <adriiglezs0497@gmail.com>
2023-01-19 11:43:25 -0600

    modificación para jalar dato del mes

--- index.js
+++ index.js
@@ -5,36 +5,37 @@ const indication = () => {
     let month = document.getElementById("month").value;
     let day = document.getElementById("day").value;
     document.getElementById("sign").value = month;
+    let date = new Date(year,month,day).getDay();
+    console.log(year);
+    console.log(month);
+    console.log(day);
+    console.log(date);
     
-    if (parseInt(day)>20 && month === "Marzo"|| parseInt(day)<20 && month === "Abril")
+    if (parseInt(day)>20 && month == 2|| parseInt(day)<20 && month == 3)
     document.getElementById("sign").value = "Eres el mejor Signo... Aries";
-    else if (parseInt(day)>20 && month ==="Abril"|| parseInt(day)<20 && month === "Mayo")
+    else if (parseInt(day)>20 && month ==3|| parseInt(day)<20 && month == 4)
     document.getElementById("sign").value = "No eres el mejor Signo... Tauro";
-    else if (parseInt(day)>20 && month === "Mayo"|| parseInt(day)<20 && month === "Junio")
+    else if (parseInt(day)>20 && month == 4|| parseInt(day)<20 && month == 5)
     document.getElementById("sign").value = "No eres el mejor Signo... Geminis";
-    else if (parseInt(day)>20 && month === "Junio"|| parseInt(day)<20 && month === "Julio")
+    else if (parseInt(day)>20 && month == 5|| parseInt(day)<20 && month == 6)
     document.getElementById("sign").value = "No eres el mejor Signo... Cáncer";
-    else if (parseInt(day)>20 && month === "Julio"|| parseInt(day)<20 && month === "Agosto")
+    else if (parseInt(day)>20 && month == 6|| parseInt(day)<20 && month == 7)
     document.getElementById("sign").value = "No eres el mejor Signo... Leo";
-    else if (parseInt(day)>20 && month === "Agosto"|| parseInt(day)<20 && month === "Septiembre")
+    else if (parseInt(day)>20 && month == 7|| parseInt(day)<20 && month == 8)
     document.getElementById("sign").value = "No eres el mejor Signo... Virgo";
-    else if (parseInt(day)>20 && month === "Septiembre"|| parseInt(day)<20 && month === "Octubre")
+    else if (parseInt(day)>20 && month == 8|| parseInt(day)<20 && month == 9)
     document.getElementById("sign").value = "No eres el mejor Signo... Libra";
-    else if (parseInt(day)>20 && month === "Octubre"|| parseInt(day)<20 && month === "Noviembre")
+    else if (parseInt(day)>20 && month == 9|| parseInt(day)<20 && month == 10)
     document.getElementById("sign").value = "No eres el mejor Signo... Escorpio";
-    else if (parseInt(day)>20 && month === "Noviembre"|| parseInt(day)<20 && month === "Diciembre")
+    else if (parseInt(day)>20 && month == 10|| parseInt(day)<20 && month == 11)
     document.getElementById("sign").value = "No eres el mejor Signo... Sagitario";
-    else if (parseInt(day)>20 && month === "Diciembre"|| parseInt(day)<20 && month === "Enero")
+    else if (parseInt(day)>20 && month == 11|| parseInt(day)<20 && month == 0)
     document.getElementById("sign").value = "No eres el mejor Signo... Capricornio";
-    else if (parseInt(day)>20 && month === "Enero"|| parseInt(day)<20 && month === "Febrero")
+    else if (parseInt(day)>20 && month == 0|| parseInt(day)<20 && month == 1)
     document.getElementById("sign").value = "No eres el mejor Signo... Acuario";
-    else if (parseInt(day)>20 && month === "Febrero"|| parseInt(day)<20 && month === "Marzo")
+    else if (parseInt(day)>20 && month == 1|| parseInt(day)<20 && month == 2)
     document.getElementById("sign").value = "No eres el mejor Signo... Piscis";
-    else
-    document.getElementById("sign").value = "Haz de ser Ofiuco por que no te encuentro";
-    
-
-    let date = new Date(year, month, day).getDay();
+    else document.getElementById("sign").value = "Haz de ser Ofiuco por que no te encuentro";
 
     switch (date) {
         case 0:
@@ -47,4 +48,4 @@ const indication = () => {
             document.getElementById("result").innerHTML = "Hoy si hay trabajo o escuela";
             break;
     }
-}
+};
